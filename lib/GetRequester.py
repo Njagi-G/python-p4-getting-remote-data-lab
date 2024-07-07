f1d9eb8d8fd8211a264b3409fd0058e886cf737f
Njagi Githaiga <njagigithaiga@gmail.com>
2024-07-07 15:53:04 +0300

    Complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,21 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response = json.loads(self.get_response_body())
+        return response
+
+
+# Test the class for requests, responses and response formats
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+
+result1 = get_requester.get_response_body()
+print(f"\n Result Body: {result1}\n")
+
+result2 = get_requester.load_json()
+print(f"Result as list of dictionaries: {result2}")
+
